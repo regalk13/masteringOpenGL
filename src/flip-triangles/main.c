c6e763fd925d4a43a6a2c19157cd225f741c2368
Regalk <jsalazarcaviedes30@gmail.com>
2023-01-31 19:13:42 -0500

    Flip triangles example

---
+++ src/flip-triangles/main.c
@@ -0,0 +1,165 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
+
+
+#include "glad.c"
+
+#define GLFW_INCLUDE_GLEXT
+#include <GLFW/glfw3.h>
+
+#define DEFAULT_SCREEN_WIDTH 1600
+#define DEFAULT_SCREEN_HEIGHT 900
+
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+  "out vec4 FragColor;\n" 
+  "void main()\n"
+  "{\n"
+  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}\0";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+int main(void) { 
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not initialize GLFW\n");
+        exit(1);
+    }
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
+    GLFWwindow * const window = glfwCreateWindow(
+            DEFAULT_SCREEN_WIDTH, 
+            DEFAULT_SCREEN_HEIGHT, 
+            "Fractal", 
+            NULL, 
+            NULL);
+    
+    if (window == NULL) {
+        fprintf(stderr, "ERROR: the window could not be created");
+        glfwTerminate();
+        exit(1);
+    }
+
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "ERROR: could not initialize GLAD\n");
+        exit(1);
+    }
+    printf("Opengl used in this platform (%s): \n", glGetString(GL_VERSION));
+    glViewport(0, 0 , DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
+
+    // Vertex Shaders
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success; 
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+       glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+       fprintf(stderr, "ERROR: shader vertex compilation failed: %c\n", infoLog);
+     }
+    
+    // Fragment Shaders
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog); 
+      fprintf(stderr, "ERROR: fragment shader compilation failed: %c\n", infoLog);
+    }
+     
+    // Link shaders, shader program
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success) {
+      glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog); 
+      fprintf(stderr, "ERROR: shader program link failed\n");
+    }
+
+    // Delete shaders don't needed the program now have the memory itself
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    unsigned int VAO, VBO;
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    }; 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+    while(!glfwWindowShouldClose(window)) {
+      // input commands
+        processInput(window);
+
+        // Rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // CHeck and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    
+    glfwTerminate();
+    return 0;
+}
